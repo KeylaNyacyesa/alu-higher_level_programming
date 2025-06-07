ecd6fdf5cda65572a8c686ef178fbb5164ddebc9
KeylaNyacyesa <k.nyacyesa@alustudent.com>
2025-06-07 13:53:49 -0700

    Niq

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+//JavaScript script that updates the text color of the header to red when clicked on
+// using the jQuery API
+$('#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
